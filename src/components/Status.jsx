ee9a4652425604e9e7c5d4ee16b0a4f5fb8f5a07
Charlie Cooper <charliepeople427@gmail.com>
2025-12-31 15:45:59 -0800

    gacp

--- src/components/Status.jsx
+++ src/components/Status.jsx
@@ -1,10 +1,43 @@
 import '../css/Controls.css'
+import { useState } from 'react'
+
+// const ToggleSwitch = ({ label, leftLabel = "Control Panel", rightLabel = "LLM Interface" }) => {
+//     const [isChecked, setIsChecked] = useState(false);
+
+//     const toggleSwitch = () => {
+//         setIsChecked(!isChecked);
+//     };
+
+//     return (
+//         <div className="toggle-container">
+//             <span className={`toggle-label ${!isChecked ? 'active' : ''}`}>
+//                 {leftLabel}
+//             </span>
+//             <div className="toggle-switch">
+//                 <input
+//                     type="checkbox"
+//                     className="checkbox"
+//                     name={label}
+//                     id={label}
+//                     checked={isChecked}
+//                     onChange={toggleSwitch}
+//                 />
+//                 <label className="label" htmlFor={label}>
+//                     <span className="switch" />
+//                 </label>
+//             </div>
+//             <span className={`toggle-label ${isChecked ? 'active' : ''}`}>
+//                 {rightLabel}
+//             </span>
+//         </div>
+//     );
+// };
 
 export default function Status({vehicleCount}) {
     return (
         <div className="status">
-            <p>vehicle count: {vehicleCount}</p>
-            <p>route count</p>
+            <p><b>vehicle count:</b> {vehicleCount}</p>
+            {/* <ToggleSwitch label="interface-toggle" />  Add label prop */}
         </div>
     );
 }
